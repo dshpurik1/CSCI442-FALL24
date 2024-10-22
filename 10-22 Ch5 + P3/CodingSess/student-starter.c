f8f1564c6c4b84c080c88e571da28b0126f0894c
Danil Shpurik <dshpurik2@gmail.com>
2024-10-22 15:22:28 -0600

    10-22 Starter Code

---
+++ 10-22 Ch5 + P3/CodingSess/student-starter.c
@@ -0,0 +1,44 @@
+
+#include <pthread.h>
+#include <stdio.h>
+
+// General Tips:
+//    1. Don't worry about memory safety. Screw it up however you want as long as you get the results.
+//    2. Reference man pages for what parameters api calls need
+//    3. Feel free to work with your neighbors and ask questions!
+//
+//
+//
+// Challenge 1: 
+//    1. Write a program that creates two threads. 
+//       Each thread should print a unique message ("Thread 1" and "Thread 2").
+//    2. Your program will have a critical section of code where you can pass arguments.
+//    3. Use pthread_create and pthread_join to kick off multiple threads and to join them back together.   
+//    4. Your printing code should look like this `printf("Thread Message: %s\n", (char*)arg);`
+//             
+// 
+// Challenge 2:
+//    1. Modify the previous program to pass a structure containing an integer and a string to each thread. 
+//       The thread should print both values.
+//
+//
+// Challenge 3:
+//    1. Write a program where any number of threads specified increment a shared counter.
+//    2. Synchronization does not matter here.
+//    3. You should convert your program to allow for NUm_THREADS to be created instead of just two.
+//
+//
+// Challenge 4:
+//    1. Modify the previous program so that the shared counter increments correctly.
+//    2. Synchronization DOES matter so use mutexes here.
+
+
+void* print_message(void* arg) {
+    
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
